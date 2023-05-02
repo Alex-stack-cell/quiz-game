1977643cb92580c557451ade99073c2097fa5357
AlexPixel <alexandregavriilidis@gmail.com>
2023-05-02 22:08:58 +0200

    Install axios and test it

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,17 @@
 <script>
 export default {
   name: 'App',
+
+  created() {
+    const api = 'https://opentdb.com/api.php?amount=1&category=18&difficulty=easy';
+
+      this.axios
+        .get(api)
+        .then((response) => {
+          console.log(response.data.results[0])
+        });
+    
+  }
 }
 </script>
 <style lang="scss">
